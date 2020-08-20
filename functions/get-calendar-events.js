265e1e9801a0983d0ee8f0248024f31400ae8356
Mario Andres <marioarendon2@gmail.com>
2020-08-19 22:09:45 -0600

    get calendar events

--- functions/get-calendar-events.js
+++ functions/get-calendar-events.js
@@ -1,16 +1,20 @@
-require('dotenv').config()
-const axions = require('axios')
+const axios = require('axios')
 
-exports.handler = async () => {
+exports.handler = async (event, context) => {
   try {
-    const result = await axions({
-      url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_EMAIL}/events`,
+    const result = await axios({
+      url: `${process.env.API_URL}/get-access-token`,
       method: 'GET',
-      headers: {
-        Authorization: `Bearer `,
-      },
+    }).then(({ data }) => {
+      return axios({
+        url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_EMAIL}/events`,
+        method: 'GET',
+        headers: {
+          Authorization: `Bearer ${data.access_token}`,
+        },
+      })
     })
-    console.log('result', result.data.items)
+
     return {
       statusCode: 200,
       body: JSON.stringify({ events: result.data.items }),
