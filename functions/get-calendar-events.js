3e31924450fb507b350d4413844cb2658985f563
Mario Andres <marioarendon2@gmail.com>
2020-08-27 20:28:23 -0600

    Add queries to get all events request

--- functions/get-calendar-events.js
+++ functions/get-calendar-events.js
@@ -1,4 +1,5 @@
 const axios = require('axios')
+const MAX_RESULTS = 500
 
 exports.handler = async (event, context) => {
   try {
@@ -7,7 +8,7 @@ exports.handler = async (event, context) => {
       method: 'GET',
     }).then(({ data }) => {
       return axios({
-        url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_EMAIL}/events`,
+        url: `https://www.googleapis.com/calendar/v3/calendars/${process.env.CALENDAR_EMAIL}/events?singleEvents=true&maxResults=${MAX_RESULTS}&orderBy=startTime`,
         method: 'GET',
         headers: {
           Authorization: `Bearer ${data.access_token}`,
